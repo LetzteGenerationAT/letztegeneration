b9128b420e8f990e18d4b3c51908a32380b64b5f
Philipp R. Proksch <mail@prp.codes>
2023-03-04 19:09:45 +0100

    refactored

--- src/components/Card/TitleCard.tsx
+++ src/components/Card/TitleCard.tsx
@@ -14,23 +14,25 @@ function TitleCard({
   return (
     <div
       className={
-        "card bg-base-100 w-full p-6 shadow-xl " + (topMargin || "mt-6")
+        "card w-full bg-base-100 p-6 shadow-xl " + (topMargin || "mt-6")
       }
     >
       {/* Title for Card */}
       <Subtitle className={TopSideButtons ? "inline-block" : ""}>
         {title}
 
-        {/* Top side button, show only if present */}
         {TopSideButtons && (
+          /* Top side button, show only if present */
           <div className="float-right inline-block">{TopSideButtons}</div>
         )}
       </Subtitle>
 
       <div className="divider mt-2"></div>
 
-      {/** Card Body */}
-      <div className="bg-base-100 h-full w-full pb-6">{children}</div>
+      {children && (
+        /** Card Body */
+        <div className="h-full w-full bg-base-100 pb-6">{children}</div>
+      )}
     </div>
   );
 }
